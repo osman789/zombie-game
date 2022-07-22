7d137db5d9f1bd5328b5f6bab18a854f0ac56a79
Jorge Perez <jp10791@hotmail.com>
2022-07-22 14:07:25 -0400

    added background sound

--- Zombie Game/Properties/Resources.Designer.cs
+++ Zombie Game/Properties/Resources.Designer.cs
@@ -91,6 +91,16 @@ namespace Zombie_Game.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
+        internal static byte[] gamebackground {
+            get {
+                object obj = ResourceManager.GetObject("gamebackground", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap left {
